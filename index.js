a712d5ff86aa9c898a01b302274a417d186d998f
KernelDeimos <eric.alex.dube@gmail.com>
2025-01-08 17:19:18 -0500

    try: maybe workspaces causes it

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-const { LRUCache: LRU } = require('lru-cache');
-const test = new LRU({
-    max: 50,
-    maxAge: 1000 * 60,
-});
-console.log('\x1B[32;1m--- REACHED END ---\x1B[0m');
+const lruUser = require('lru-user');
+lruUser.run();
---
+++ src/lru-user/index.js
@@ -0,0 +1,11 @@
+const { LRUCache: LRU } = require('lru-cache');
+
+module.exports = {
+    run: () => {
+        const test = new LRU({
+            max: 50,
+            maxAge: 1000 * 60,
+        });
+        console.log('\x1B[32;1m--- REACHED END ---\x1B[0m');
+    },
+};
